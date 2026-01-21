ec964aaf555207dfaaf4fb607810aaa78707308f
z4qu <59421804+z4qu@users.noreply.github.com>
2026-01-21 18:08:34 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task('buildjs', function(){
-    gulp.src('source/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+gulp.task('buildjs', function () {
+    return gulp.src('source/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
